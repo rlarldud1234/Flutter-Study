3a86e6edb1ffa1bd3b22e3550afcfa4fff48a675
rlarldud1234 <rlarldud6482@naver.com>
2022-12-14 19:02:02 +0900

    feat :: colors 세팅

---
+++ todo_list/lib/src/resource/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color red = Color(0xFFDA4040);
+const Color blue = Color(0xFF5F52EE);
+
+const Color black = Color(0xFF3A3A3A);
+const Color gray = Color(0xFF717171);
+
+const Color bgColor = Color(0xFFEEEFF5);
